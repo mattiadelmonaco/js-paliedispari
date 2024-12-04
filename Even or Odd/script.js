db9b2ad73a05673a74a39678e60f5aefe349e383
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-04 21:40:23 +0100

    feat: add steps

--- Even or Odd/script.js
+++ Even or Odd/script.js
@@ -8,3 +8,17 @@
 
 // STEPS:
 
+// 1. L'utente sceglie pari o dispari (prompt dove inserire "pari" o "dispari")
+//    - Creare variabili con "pari" = true e "dispari" = false
+//    - SE la parola inserita è diversa dalla parola "pari" o "dispari" 
+//      alert per avvisare dell'errore e torna alla scelta iniziale
+// 2. L'utente inserisce un numero tra 1 e 5 (prompt con inserimento numero 1 >= 5)
+//    - SE il numero inserito non è compreso tra 1 e 5 oppure è una parola 
+//      alert per avvisare dell'errore e torna alla scelta iniziale
+// 3. Creare funzione per generare un numero tra 1 e 5 per il computer
+// 4. Somma dei due numeri (numero scelto utente + nome funzione)
+// 5. Creare funzione per stabilire se il risultato è pari (true) o dispari (false)
+// 6. Alert per indicare se ha vinto l'utente oppure il computer
+
+// CODE: 
+
