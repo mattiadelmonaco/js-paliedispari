05918b0c4de064491e1b489e7810591aeb34fe74
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-04 16:32:15 +0100

    feat:: add steps

--- Palindrome Words/script.js
+++ Palindrome Words/script.js
@@ -5,3 +5,13 @@
 
 // STEPS:
 
+// 1. Inserire prompt per far inserire all'utente una parola
+// 2. Creare e utilizzare una funzione con risultato booleano per capire se è palindroma o no 
+// 3. Creare variabile booleana true o false
+//    - SE la parola inserita è === a quella parola con reverse (da fare passo passo e non con il metodo ".reverse") è true
+//      - alert con scritto "è palindroma"
+//    - ALTRIMENTI SE la parola inserita è !== a quella parola con reverse è false
+//      - alert con scritto "non è palindroma"
+
+// CODE:
+
